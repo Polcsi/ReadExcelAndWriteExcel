da66599a9129aa582bbbc6e7c44999f0c3da942e
Polcsi <pollakbence12@gmail.com>
2022-05-31 21:42:38 +0200

    Add project files.

---
+++ ReadExcelAndWriteExcel/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+
+namespace ReadExcelAndWriteExcel
+{
+    class Program
+    {
+        private List<Town> towns = new List<Town>();
+        static void Main(string[] args)
+        {
+            Console.Write("Give an excel file path: ");
+            string path = Console.ReadLine();
+
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File Exists.");
+
+                Program prg = new Program();
+
+                try
+                {
+                    File.ReadAllLines(path, Encoding.Default).ToList().ForEach(delegate (string line)
+                    {
+                        prg.towns.Add(new Town(line));
+                    });
+                    Console.WriteLine("File Read!");
+                    writeExcelFile(prg.towns, "hely2");
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.Message);
+                }
+
+            } else
+            {
+                Console.WriteLine("File does not Exist!");
+            }
+
+            Console.ReadKey();
+        }
+        public static void writeExcelFile<T>(List<T> collection, string filename) where T : Town
+        {
+            string outPutDir = $"{Directory.GetCurrentDirectory()}/excel";
+            if(!Directory.Exists(outPutDir))
+            {
+                Directory.CreateDirectory(outPutDir);
+            }
+            try
+            {
+                Console.WriteLine("Writing File...");
+                Application oXL;
+                _Workbook oWB;
+                _Worksheet oSheet;
+                Range oRng;
+                object misvalue = System.Reflection.Missing.Value;
+
+                // Start Excel and get Application object.
+                oXL = new Application();
+                oXL.Visible = true;
+
+                //Get a new workbook.
+                oWB = (_Workbook)(oXL.Workbooks.Add(""));
+                oSheet = (_Worksheet)oWB.ActiveSheet;
+
+                //Add table headers going cell by cell.
+                oSheet.Cells[1, 1] = "helysegnev";
+                oSheet.Cells[1, 2] = "tipus";
+                oSheet.Cells[1, 3] = "megye";
+                oSheet.Cells[1, 4] = "jaras";
+                oSheet.Cells[1, 5] = "terulet";
+                oSheet.Cells[1, 6] = "lakossag";
+                oSheet.Cells[1, 7] = "lakasokszama";
+
+                //Format A1:G1 as bold, vertical alignment = center.
+                oSheet.get_Range("A1", "G1").Font.Bold = true;
+                oSheet.get_Range("A1", "G1").VerticalAlignment =
+                    XlVAlign.xlVAlignCenter;
+
+                for (int i = 0; i < collection.Count; ++i)
+                {
+                    oSheet.Cells[i + 2, 1] = collection[i].Name;
+                    oSheet.Cells[i + 2, 2] = collection[i].Type;
+                    oSheet.Cells[i + 2, 3] = collection[i].County;
+                    oSheet.Cells[i + 2, 4] = collection[i].District;
+                    oSheet.Cells[i + 2, 5] = collection[i].Area;
+                    oSheet.Cells[i + 2, 6] = collection[i].Population;
+                    oSheet.Cells[i + 2, 7] = collection[i].ApartmentsCount;
+                }
+
+                //AutoFit columns A:G.
+                oRng = oSheet.get_Range("A1", "G1");
+                oRng.EntireColumn.AutoFit();
+
+                oXL.Visible = false;
+                oXL.UserControl = false;
+                oWB.SaveAs($"{outPutDir}/{filename}.xls", XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                    false, false, XlSaveAsAccessMode.xlNoChange,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                oWB.Close();
+                oXL.Quit(); 
+                Console.WriteLine("File Created!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ReadExcelAndWriteExcel/Town.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadExcelAndWriteExcel
+{
+    public class Town
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string County { get; set; }
+        public string District { get; set; }
+        public int Area { get; set; }
+        public int Population { get; set; }
+        public int ApartmentsCount { get; set; }
+        public Town(string line)
+        {
+            string[] lineData = line.Split(';');
+            Name = lineData[0];
+            Type = lineData[1];
+            County = lineData[2];
+            District = lineData[3];
+            Area = int.Parse(lineData[4]);
+            Population = int.Parse(lineData[5]);
+            ApartmentsCount = int.Parse(lineData[6].Substring(0, lineData[6].Length - 1));
+        }
+        public override string ToString()
+        {
+            return string.Format($"{Name};{Type};{County};{District};{Area};{Population};{ApartmentsCount}");
+        }
+    }
+}
